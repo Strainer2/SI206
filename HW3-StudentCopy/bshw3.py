e99a74ac2f90b0d3b940b7d2064b48746c0a315b
John Strainer <JBone@Johns-MacBook-Pro-2.local>
2016-11-08 19:13:34 -0500

    Election night commit

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -10,12 +10,34 @@
 
 # Deliverables
 # Make sure the new page is uploaded to your GitHub account.
-import urllib.request, urllib.parse, urllib.error
+# Use https://www.si.umich.edu/programs/bachelor-science-information/bsi-admissions
+
+# STEPS 
+# Create a similar HTML file but 
+# 1) Replace every occurrence of the word “student” with “AMAZING
+# student.”  
+# 2) Replace the main picture with a picture of yourself.
+# 3) Replace any local images with the image I provided in media.  (You
+# must keep the image in a separate folder than your html code.
+
+# Deliverables
+# Make sure the new page is uploaded to your GitHub account.
+
+import requests
+import re
 from bs4 import BeautifulSoup
 
-url= https://www.si.umich.edu/programs/bachelor-science-information/bsi-admissions
-html= urllib.request.urlopen(url).read()
-soup= BeautifulSoup(html, 'html.parser')
+print
+print (" - ....working......")
+print
 
-x=soup.findAll('students', limit=10)
-print(x)
+base_url = 'https://www.si.umich.edu/programs/bachelor-science-information/bsi-admissions'
+r = requests.get(base_url)
+soup = BeautifulSoup(r.text, "html.parser")
+
+# Part 1
+student = soup.find_all(text = re.compile('student'))
+for word in student:
+    fixed_text = str(word).replace('student', 'AMAZING student')
+    word.replace_with(fixed_text)
+
